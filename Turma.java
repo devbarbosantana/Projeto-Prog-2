9d921ffb304eca82474bbfacbb7f9852bff420e4
Jair Barbosa Santana <93272435+devbarbosantana@users.noreply.github.com>
2026-05-06 17:01:48 -0300

    Update Turma.java
    Código feito 100% com IA, utilizado como possível base e será alterado no futuro

--- Turma.java
+++ Turma.java
@@ -1,1 +1,29 @@
+public class Turma {
+    private int idTurma;
+    private Curso curso;
+    private Professor professor;
+    
+    // Arrays para notas e faltas (relacionados aos alunos)
+    // Para simplificar no array puro, podemos usar arrays paralelos onde o índice representa um aluno matriculado
+    private Aluno[] alunosMatriculados;
+    private double[][] notas; // Ex: notas[indiceAluno][numeroDaNota]
+    private int[] faltas;     // Ex: faltas[indiceAluno]
+    
+    private int quantidadeAlunos;
+    private final int CAPACIDADE_MAXIMA = 50; // Limite padrão
 
+    public Turma(int idTurma, Cadeira cadeira, Sala sala, Professor professor) {
+        this.idTurma = idTurma;
+        this.cadeira = cadeira;
+        this.sala = sala;
+        this.professor = professor;
+        
+        this.alunosMatriculados = new Aluno[CAPACIDADE_MAXIMA];
+        this.notas = new double[CAPACIDADE_MAXIMA][3]; // Supondo 3 notas por aluno
+        this.faltas = new int[CAPACIDADE_MAXIMA];
+        this.quantidadeAlunos = 0;
+    }
+
+    // Métodos para matricular aluno, lançar nota e registrar falta viriam aqui...
+    // Getters e Setters...
+}
